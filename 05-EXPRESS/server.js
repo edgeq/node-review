2429e84af8fabbabe91fe350a1ffa1366e386e03
edgeq <edgarq86@gmail.com>
2019-05-29 12:31:30 -0500

    connected to mongodb and got introfuced to async patterns

--- 05-EXPRESS/server.js
+++ 05-EXPRESS/server.js
@@ -4,23 +4,50 @@ const app = express();
 const port = 8888;
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const mongoose = require('mongoose');
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-let messages = [
-  {name: 'Larry', message: 'Curb Your Enthusiasm'},
-  {name: 'Jerry', message: 'A show about nothing'}
-];
+// define the db connection string
+const dbUrl = 'mongodb+srv://user:user@mongedge-4r0of.mongodb.net/test?retryWrites=true';
+// set up a data model
+const Message = mongoose.model('Message', {
+  name: String,
+  message: String
+})
+
+// let messages = [
+//   {name: 'Larry', message: 'Curb Your Enthusiasm'},
+//   {name: 'Jerry', message: 'A show about nothing'}
+// ];
 
 app.get('/messages', (req, res) => {
-  res.send(messages)
+  Message.find({}, (err, messages) => {
+    res.send(messages)
+  })
 })
-app.post('/messages', (req, res) => {
-  messages.push(req.body);
-  io.emit('message', req.body);
-  res.sendStatus(200);
+
+app.post('/messages', async (req, res) => {
+
+  try {
+    let message = new Message(req.body);
+    let savedMessage = await message.save()
+    console.log('saved')
+    let censored = Message.findOne({message: 'badword'})
+
+      if(censored)
+       await Message.deleteOne({_id: censored.id })
+      else
+        io.emit('message', req.body)
+        res.sendStatus(200)
+  } catch (error) {
+    res.sendStatus(500);
+    return console.error(err);
+  } finally {
+    console.log('message post called')
+  }
 })
 
 io.on('connection', socket => {
@@ -28,6 +55,10 @@ io.on('connection', socket => {
   socket.on('disconnect', () => {
     console.log('user disconnected');
   })
+})
+
+mongoose.connect(dbUrl, {useNewUrlParser: true}, (err) => {
+  console.log('mongoDB connection', err);
 })
 
 http.listen(port, () => {
